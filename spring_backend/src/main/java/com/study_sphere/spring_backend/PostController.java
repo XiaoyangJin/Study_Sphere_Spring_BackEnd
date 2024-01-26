004f79c7c9e68767a7c04d569d518de3fbc70287
XiaoyangJin <jin.xiaoya@northeastern.edu>
2024-01-26 14:47:50 -0500

    Delete REST API by id

--- spring_backend/src/main/java/com/study_sphere/spring_backend/PostController.java
+++ spring_backend/src/main/java/com/study_sphere/spring_backend/PostController.java
@@ -1,12 +1,15 @@
 package com.study_sphere.spring_backend;
 
 import java.util.List;
+import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -30,5 +33,17 @@ public class PostController {
     public ResponseEntity<List<Post>> getAllPosts() {
         List<Post> posts = postRepository.findAll();
         return new ResponseEntity<>(posts, HttpStatus.OK);
+    }
+	
+	@DeleteMapping("/{id}")
+    public ResponseEntity<Void> deletePost(@PathVariable Long id) {
+        Optional<Post> post = postRepository.findById(id);
+
+        if (post.isPresent()) {
+            postRepository.delete(post.get());
+            return new ResponseEntity<>(HttpStatus.NO_CONTENT);
+        } else {
+            return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+        }
     }
 }
