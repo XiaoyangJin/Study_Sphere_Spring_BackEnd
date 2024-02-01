44de5747aed4b62ea19819315a3305d0e044c745
XiaoyangJin <jin.xiaoya@northeastern.edu>
2024-01-31 20:30:56 -0500

    Get All Posts Test

--- spring_backend/src/test/java/com/study_sphere/spring_backend/PostControllerTests.java
+++ spring_backend/src/test/java/com/study_sphere/spring_backend/PostControllerTests.java
@@ -18,6 +18,7 @@ import org.springframework.test.web.servlet.MockMvc;
 
 // manually import
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
 import static org.hamcrest.Matchers.is;
@@ -36,7 +37,7 @@ class PostControllerTests {
 	private PostRepository postRepository;
 
 	@Test
-	public void testCreatePost() throws Exception {
+	public void createPostTest() throws Exception {
 	    Post post = new Post(100, "Test Post Title", "Test Post Summary", "Test Post Content");
 	    Mockito.when(postRepository.save(any(Post.class))).thenReturn(post);
 
@@ -46,5 +47,18 @@ class PostControllerTests {
 	            .andExpect(status().isCreated())
 	            .andExpect(jsonPath("$.title", is(post.getTitle())));
 	}
+	
+	@Test
+	public void getAllPostsTest() throws Exception {
+	    List<Post> posts = Arrays.asList(new Post(101, "Test Get Title 1", "Test Get Summary 1", "Test Get Content 1"),
+	                                     new Post(102, "Test Get Title 2", "Test Get Summary 2", "Test Get Content 2"));
+	    given(postRepository.findAll()).willReturn(posts);
+
+	    mockMvc.perform(get("/api/posts")
+	            .contentType(MediaType.APPLICATION_JSON))
+	            .andExpect(status().isOk())
+	            .andExpect(jsonPath("$.size()").value(posts.size()));
+	}
+
 
 }
