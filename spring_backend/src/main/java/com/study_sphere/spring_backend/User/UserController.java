a830efc4eca0be0c82ec89464145ec81e544148e
XiaoyangJin <jin.xiaoya@northeastern.edu>
2024-02-05 19:59:01 -0500

    User GET Api

--- spring_backend/src/main/java/com/study_sphere/spring_backend/User/UserController.java
+++ spring_backend/src/main/java/com/study_sphere/spring_backend/User/UserController.java
@@ -1,14 +1,18 @@
 package com.study_sphere.spring_backend.User;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.study_sphere.spring_backend.Post.Post;
 import com.study_sphere.spring_backend.User.User;
 import com.study_sphere.spring_backend.User.UserRepository;
 
@@ -24,4 +28,10 @@ public class UserController {
 		User savedUser = userRepository.save(user);
         return new ResponseEntity<>(savedUser, HttpStatus.CREATED);	
 	}
+	
+	@GetMapping
+    public ResponseEntity<List<User>> getAllUsers() {
+        List<User> users = userRepository.findAll();
+        return new ResponseEntity<>(users, HttpStatus.OK);
+    }
 }
