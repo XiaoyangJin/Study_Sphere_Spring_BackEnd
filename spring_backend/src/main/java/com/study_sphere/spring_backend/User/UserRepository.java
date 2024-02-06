0e1712518b05ffec2167347e4dc262efd9466599
XiaoyangJin <jin.xiaoya@northeastern.edu>
2024-02-05 19:52:46 -0500

    create user repository

--- spring_backend/src/main/java/com/study_sphere/spring_backend/User/UserRepository.java
+++ spring_backend/src/main/java/com/study_sphere/spring_backend/User/UserRepository.java
@@ -1,5 +1,9 @@
 package com.study_sphere.spring_backend.User;
 
-public interface UserRepository {
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.rest.core.annotation.RepositoryRestResource;
+
+@RepositoryRestResource(collectionResourceRel = "people", path = "people")
+public interface UserRepository extends JpaRepository<User, Long> {
 
 }
