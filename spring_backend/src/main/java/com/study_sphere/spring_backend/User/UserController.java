0f536051cd4e34fcbe34163760af5c0dc84404cd
XiaoyangJin <jin.xiaoya@northeastern.edu>
2024-02-05 20:04:44 -0500

    User Update/PUT Api

--- spring_backend/src/main/java/com/study_sphere/spring_backend/User/UserController.java
+++ spring_backend/src/main/java/com/study_sphere/spring_backend/User/UserController.java
@@ -11,6 +11,7 @@ import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -48,5 +49,19 @@ public class UserController {
         } else {
             return new ResponseEntity<>(HttpStatus.NOT_FOUND);
         }
+    }
+	
+	@PutMapping("/{id}")
+    public ResponseEntity<User> updateUser(@PathVariable Long id, @RequestBody User userDetails) {
+        return userRepository.findById(id)
+            .map(user -> {
+                // Update the fields of 'user' using 'userDetails'
+                user.setUsername(userDetails.getUsername());
+                user.setPassword(userDetails.getPassword());
+
+                User updatedUser = userRepository.save(user);
+                return new ResponseEntity<>(updatedUser, HttpStatus.OK);
+            })
+            .orElseGet(() -> new ResponseEntity<>(HttpStatus.NOT_FOUND));
     }
 }
